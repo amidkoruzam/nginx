71df6751b99141a2042377d3d4e4f103e719d945
Dmitrii Mazurok <tmazurok@gmail.com>
2023-02-28 22:51:00 +0300

    edit self signed cert generation

--- scripts/certificate.sh
+++ scripts/certificate.sh
@@ -1,1 +1,11 @@
-openssl req -x509 -nodes -newkey rsa:4096 -keyout ../certs/key.pem -out ../certs/cert.pem -days 365 -subj '/CN=localhost'
+# LibreSSL
+openssl req \
+    -x509 \
+    -nodes \
+    -newkey rsa:4096 \
+    -keyout ./certs/key.pem \
+    -out ./certs/cert.pem \
+    -days 365 \
+    -subj "/CN=localhost" \
+    -extensions san \
+    -config ./scripts/openssl.cnf
